f0e1995af4394e819c9ae41acbf6e6d4033e3a6a
AlexPetrova <a.petrova95@yahoo.com>
2020-02-01 13:07:02 +0200

    add bootstrap and jquery

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { WeatherInfoComponent } from './weather-info/weather-info.component';
+import { WeatherDataService } from './service';
+import { DateFormatPipe } from './pipe/date-format.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherCardComponent
+    WeatherCardComponent,
+    WeatherInfoComponent,
+    DateFormatPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [WeatherDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
