9354db25b0f071b8192454a3572d8ff0d37c0d84
AlexPetrova <a.petrova95@yahoo.com>
2020-02-01 13:05:32 +0200

    add service and mock data

---
+++ app/src/app/service/WeatherDataService.ts
@@ -0,0 +1,18 @@
+import { WeatherAPIResponse } from "../../types";
+import { WeatherResponseMock } from './mock';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class WeatherDataService {
+    apiUrl: string;
+
+    constructor() {
+        this.apiUrl = 'apiUrl';
+    }
+
+    async getData(): Promise<WeatherAPIResponse> {
+        return WeatherResponseMock;
+        // let response = await fetch(this.apiUrl);
+        // return response.json();
+    }
+}
