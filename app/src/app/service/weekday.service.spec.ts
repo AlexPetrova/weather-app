1fe96cb9dd93f0a6cbee94b67b80fb17cf405a72
AlexPetrova <a.petrova95@yahoo.com>
2020-02-15 13:18:14 +0200

    update WeekDatService to use the MomentService
    add description for the functions in the service
    add unit test

--- app/src/app/service/weekday.service.spec.ts
+++ app/src/app/service/weekday.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WeekdayService } from './weekday.service';
+import { MomentService } from './moment.service';
+import { Weekdays } from '../../types';
 
 describe('WeekdayService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  it('should return the current day name of the week', () => {
+    const momentService = new MomentService();
+    spyOn(momentService, 'isoWeek').and.returnValue(1);
+    const service = new WeekdayService(momentService);
 
-  it('should be created', () => {
-    const service: WeekdayService = TestBed.get(WeekdayService);
-    expect(service).toBeTruthy();
+    const currentDay = service.getCurrentDayName();
+
+    expect(currentDay).toBe(Weekdays[1])
+  });
+
+  it('should return the current day name from time stamp', () => {
+    const momentService = new MomentService();
+    spyOn(momentService, 'isoWeekday').and.returnValue(1);
+    const service = new WeekdayService(momentService);
+
+    const day = service.getDayName(1580515200);
+    expect(day).toBe(Weekdays[1])
   });
 });
--- app/src/app/service/weekday.service.ts
+++ app/src/app/service/weekday.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
 import * as moment from 'moment';
 import { Weekdays } from '../../types';
+import { MomentService } from './moment.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeekdayService {
 
+  constructor(private momentService: MomentService) { }
+
+  /**
+   * Returns the name of the week day  
+   */
   getCurrentDayName(): string {
-    let dayNumber = moment().isoWeekday();
+    let dayNumber = this.momentService.isoWeek();
     return Weekdays[dayNumber];
   }
 
-  getDayNameFromTimeStamp(timeStamp: number) {
-    let dayNumber = moment.unix(timeStamp).isoWeekday();
-    return Weekdays[dayNumber]
+  /**
+   * Returns the name of the week day from @timeStamp
+   * 
+   * @param timeStamp the date from which the name will be extracted 
+   * @returns weekday as string
+   */
+  getDayName(timeStamp: number) {
+    let dayNumber = this.momentService.isoWeekday(timeStamp);
+    return Weekdays[dayNumber];
   }
 }
