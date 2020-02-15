c6d74e9508926e11fa26a151da34e62cda0931cb
AlexPetrova <a.petrova95@yahoo.com>
2020-02-15 13:17:26 +0200

    add description for UnitConverterPipe
    add unit test

--- app/src/app/pipe/unit-converter.pipe.spec.ts
+++ app/src/app/pipe/unit-converter.pipe.spec.ts
@@ -1,8 +1,20 @@
 import { UnitConverterPipe } from './unit-converter.pipe';
+import { TemperatureUnit } from 'src/types';
 
 describe('UnitConverterPipe', () => {
-  it('create an instance', () => {
+  it('converts passed value in Celsius to Fahrenheit', () => {
     const pipe = new UnitConverterPipe();
-    expect(pipe).toBeTruthy();
+
+    const temp = pipe.transform(10, TemperatureUnit.Fahrenheit);
+
+    expect(temp).toBe(50);
+  });
+
+  it('converts passed value in Fahrenheit in Celsius', () => {
+    const pipe = new UnitConverterPipe();
+
+    const temp = pipe.transform(50, TemperatureUnit.Celsius);
+
+    expect(temp).toBe(10);
   });
 });
--- app/src/app/pipe/unit-converter.pipe.ts
+++ app/src/app/pipe/unit-converter.pipe.ts
@@ -6,6 +6,10 @@ import { TemperatureUnit } from '../../types';
 })
 export class UnitConverterPipe implements PipeTransform {
 
+  /**
+   * @param value the number representation of the temperature that will converted
+   * @param unit the unit in which the temperature will be converted
+   */
   transform(value: number, unit: TemperatureUnit): number {
     if (unit === TemperatureUnit.Celsius) {
       return value;
@@ -14,5 +18,4 @@ export class UnitConverterPipe implements PipeTransform {
       return value * 1.8 + 32;
     }
   }
-
 }
