d9c7e12179e53d782bb43d3181ad5e870efab247
AlexPetrova <a.petrova95@yahoo.com>
2020-02-15 13:16:25 +0200

    update DateFormatPipe to use MomentService
    add unit test

--- app/src/app/pipe/date-format.pipe.spec.ts
+++ app/src/app/pipe/date-format.pipe.spec.ts
@@ -1,8 +1,14 @@
 import { DateFormatPipe } from './date-format.pipe';
+import { MomentService } from '../service/moment.service';
 
 describe('DateFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateFormatPipe();
-    expect(pipe).toBeTruthy();
+  it('returns date in the passed string format', () => {
+    const momentService = new MomentService();
+    spyOn(momentService, 'format').and.returnValue('01/2/2020');
+    const pipe = new DateFormatPipe(momentService);
+
+    let date = pipe.transform(1580515200, 'DD/M/YYYY');
+
+    expect(date).toBe('01/2/2020');
   });
 });
--- app/src/app/pipe/date-format.pipe.ts
+++ app/src/app/pipe/date-format.pipe.ts
@@ -1,11 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
+import { MomentService } from '../service/moment.service';
 
 @Pipe({
   name: 'dateFormat'
 })
 export class DateFormatPipe implements PipeTransform {
+
+  constructor(private momentService: MomentService) { }
+
+  /**
+   * Transforms number representation of date to custom format 
+   * @param value date as number 
+   * @param format string format of the date - example DD/MM/YYYY
+   * @returns string date represented in the passed format
+   */
   transform(value: number, format: string): string {
-    return moment.unix(value).format(format);
+    return this.momentService.format(value, format);
   }
 }
