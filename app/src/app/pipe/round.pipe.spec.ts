53cd2d8320d18272a8dd3299f391a58dfddc294d
AlexPetrova <a.petrova95@yahoo.com>
2020-02-15 13:16:54 +0200

    add description for RoundPipe
    add unit test

--- app/src/app/pipe/round.pipe.spec.ts
+++ app/src/app/pipe/round.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { RoundPipe } from './round.pipe';
 
 describe('RoundPipe', () => {
-  it('create an instance', () => {
+  it('returns the passed number rounded', () => {
     const pipe = new RoundPipe();
-    expect(pipe).toBeTruthy();
+
+    let roundedNum = pipe.transform(1.7);
+
+    expect(roundedNum).toBe(2);
+
+    roundedNum = pipe.transform(1.001);
+
+    expect(roundedNum).toBe(1);
+
+    roundedNum = pipe.transform(1.4);
+
+    expect(roundedNum).toBe(1);
   });
 });
--- app/src/app/pipe/round.pipe.ts
+++ app/src/app/pipe/round.pipe.ts
@@ -5,6 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RoundPipe implements PipeTransform {
 
+  /**
+   * @param value number which will be rounded
+   * @returns the value rounded with Math.round JS function
+   */
   transform(value: number): number {
     return Math.round(value);
   }
